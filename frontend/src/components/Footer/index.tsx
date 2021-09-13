1fc3955aee106dfe5e9ed9530c02d8db752b70b2
Greice Castilho <greicecfarma@gmail.com>
2021-09-13 00:07:05 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/GreiceTCastilho" target="_blank" rel="noreferrer">Greice Castilho e Samuel Xavier</a></p>
+                <p className="text-light"><small><strong>Projeto em Spring React</strong><br />
+                    Para a matéria de Testes da Fatec / SJC - SP.</small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
